54a173ecb3fd58c6ef12863678a4631e618e764d
Adam Bright <adam.brian.bright@gmail.com>
2022-01-19 18:17:24 +0400

    :arrow_up: Updated dependencies

--- test.py
+++ test.py
@@ -4,9 +4,13 @@ from qrcode_styled.svg.image import SVGStylingImage
 qr = QRCodeStyled()
 
 with open('wiki/img/test.webp', 'wb') as _fh:
-    qr.get_image(data='https://cifrazia.com/ru/').save(_fh, 'WEBP', lossless=False, quality=80, method=2)
+    qr.get_image(
+        data='https://cifrazia.com/ru/',
+    ).save(_fh, 'WEBP', lossless=False, quality=80, method=2)
 
 qr = QRCodeStyled(image_factory=SVGStylingImage)
 
 with open('wiki/img/test.svg', 'wb') as _fh:
-    qr.get_image(data='https://cifrazia.com/ru/').save(_fh)
+    qr.get_image(
+        data='https://cifrazia.com/ru/',
+    ).save(_fh)
